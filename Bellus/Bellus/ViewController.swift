3cea720abd89f8a992814f910c03190825133550
Javi Gomez <rtomyj@gmail.com>
2018-01-10 23:34:39 -0600

    working on port of Bellus_Kotlin

--- Bellus/Bellus/ViewController.swift
+++ Bellus/Bellus/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func tempAction(_ sender: Any) {
+    }
 }
 
